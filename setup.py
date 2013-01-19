e2d9738e728367cc6bd698c1057ce0c717f8429c
Simeon Warner <simeon.warner@cornell.edu>
2013-01-19 00:34:19 -0500

    Change random version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='resync',
-    version='0.1dev',
+    version='0.1',
     packages=['resync',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
