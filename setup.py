172f509f602ad598da019ef4e9922896d9e6328b
Simeon Warner <simeon.warner@cornell.edu>
2013-01-21 14:04:05 -0500

    Add author metadata

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from distutils.core import setup
 setup(
     name='resync',
     version='0.1',
-    packages=['resync',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    packages=['resync'],
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Programming Language :: Python",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 "Environment :: Web Environment"],
+    author='Simeon Warner',
+    author_email='simeon.warner@cornell.edu',
     long_description=open('README.txt').read(),
+    url='http://github.com/resync/resync',
 )
