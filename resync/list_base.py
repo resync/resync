ad8b99c8d93fccbef5a95be59ceee5cc0ecf7b2e
Simeon Warner <simeon.warner@cornell.edu>
2013-02-13 14:25:02 -0500

    Add base class for ResourceList, ChangeList etc that uses ResourceContainer but adds Sitemap based IO

---
+++ resync/list_base.py
@@ -0,0 +1,70 @@
+"""Base class for ResourceSync capabilities with list of resources
+
+Adds Sitemap based IO to the ResourceContainer class and is
+intended as the base class for ResourceList, ChangeList,
+CapabilityList etc.. Adds common read() and write() methods.
+"""
+
+import collections
+import os
+from datetime import datetime
+import re
+import sys
+import StringIO
+from urllib import URLopener
+
+from resource_container import ResourceContainer
+from sitemap import Sitemap
+
+class ListBase(ResourceContainer):
+    """Class that adds Sitemap based IO to ResourceContainer
+    """
+
+    def __init__(self, resources=None, md=None, ln=None):
+        super(ListBase, self).__init__(resources=resources, md=md, ln=ln)
+        self.capability = 'unknown'
+
+    ##### INPUT #####
+
+    def read(self,uri=None,**kwargs):
+        s = Sitemap(**kwargs)
+        s.read(uri=uri,resources=self,capability=self.capability)
+        self.num_files = s.sitemaps_created
+
+    def parse(self,uri=None,fh=None,**kwargs):
+        """Parse a single XML document for this list
+
+        Does not handle the case of sitemapindex+sitemaps ResourceList
+        """
+        if (uri is not None):
+            try:
+                fh = URLopener().open(uri)
+            except IOError as e:
+                raise Exception("Failed to load sitemap/sitemapindex from %s (%s)" % (uri,str(e)))
+        if (fh is None):
+            raise Exception("Nothing to parse")
+        s = Sitemap(**kwargs)
+        s.sitemap_parse_xml(fh=fh,resources=self,capability=self.capability)
+        self.num_files = s.sitemaps_created
+
+    ##### OUTPUT #####
+
+    def as_xml(self,**kwargs):
+        """Return XML serialization of this list
+
+        FIXME - What can we do here to deal with the case where the
+        list is too big. Applies only to ResourceList since v0.5"""
+        self.default_capability_and_modified()
+        s = Sitemap(**kwargs)
+        return s.resources_as_xml(self)
+
+    def write(self,**kwargs):
+        """Write one or perhaps multiple sitemap/sitemapindex XML documents
+        """
+        self.default_capability_and_modified()
+        basename = "/tmp/resynclist.xml"
+        if ('basename' in kwargs):
+            basename = kwargs['basename']
+            del kwargs['basename']
+        s = Sitemap(**kwargs)
+        return s.write(resources=self,basename=basename)
---
+++ resync/test/test_list_base.py
@@ -0,0 +1,41 @@
+import sys
+import unittest
+import StringIO
+from resync.resource import Resource
+from resync.list_base import ListBase
+from resync.sitemap import Sitemap, SitemapIndexError
+
+# etree gives ParseError in 2.7, ExpatError in 2.6
+etree_error_class = None
+if (sys.version_info < (2,7)):
+    from xml.parsers.expat import ExpatError
+    etree_error_class = ExpatError
+else:
+    from xml.etree.ElementTree import ParseError
+    etree_error_class = ParseError
+
+class TestSitemap(unittest.TestCase):
+
+    def test_08_print(self):
+        lb = ListBase()
+        lb.add( Resource(uri='a',lastmod='2001-01-01',length=1234) )
+        lb.add( Resource(uri='b',lastmod='2002-02-02',length=56789) )
+        lb.add( Resource(uri='c',lastmod='2003-03-03',length=0) )
+        lb.md['modified']=None #avoid now being added
+        #print lb
+        self.assertEqual( lb.as_xml(), '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:rs="http://www.openarchives.org/rs/terms/"><rs:md capability="unknown" /><url><loc>a</loc><lastmod>2001-01-01T00:00:00Z</lastmod><rs:md length="1234" /></url><url><loc>b</loc><lastmod>2002-02-02T00:00:00Z</lastmod><rs:md length="56789" /></url><url><loc>c</loc><lastmod>2003-03-03T00:00:00Z</lastmod><rs:md length="0" /></url></urlset>' )
+
+    def test_11_parse_2(self):
+        xml='<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n\
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:rs="http://www.openarchives.org/rs/terms/">\
+<rs:md capability="unknown" modified="2013-02-12T14:09:00Z" />\
+<url><loc>/tmp/rs_test/src/file_a</loc><lastmod>2012-03-14T18:37:36Z</lastmod><rs:md length="12" /></url>\
+<url><loc>/tmp/rs_test/src/file_b</loc><lastmod>2012-03-14T18:37:36Z</lastmod><rs:md length="32" /></url>\
+</urlset>'
+        lb=ListBase()
+        lb.parse(fh=StringIO.StringIO(xml))
+        self.assertEqual( len(lb.resources), 2, 'got 2 resources')
+
+if  __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSitemap)
+    unittest.TextTestRunner(verbosity=2).run(suite)
