bb567d4837b8063a2de40de50baf71dfe29f423a
Simeon Warner <simeon.warner@cornell.edu>
2013-02-27 15:48:31 -0500

    Reorganized methods and chop out unused code

--- resync/sitemap.py
+++ resync/sitemap.py
@@ -60,84 +60,141 @@ class Sitemap(object):
         self.resources_created=0    # Set during parsing sitemap
         self.parsed_index=None      # Set True for sitemapindex, False for sitemap
 
-    ##### General sitemap methods that also handle sitemapindexes #####
 
-    def write(self, resources=None, basename='/tmp/sitemap.xml'):
-        """Write one or a set of sitemap files to disk
+    ##### Write the XML for a sitemap or sitemapindex #####
 
-        resources is a ResourceContainer that may be an ResourceList or
-        a ChangeList. This may be a generator so data is read as needed
-        and length is determined at the end.
-
-        basename is used as the name of the single sitemap file or the 
-        sitemapindex for a set of sitemap files.
-
-        Uses self.max_sitemap_entries to determine whether the resource_list can 
-        be written as one sitemap. If there are more entries and 
-        self.allow_multifile is set true then a set of sitemap files, 
-        with an sitemapindex, will be written.
-        """
-        # Access resources through iterator only
-        resources_iter = iter(resources)
-        ( chunk, next ) = self.get_resources_chunk(resources_iter)
-        if (next is not None):
-            # Have more than self.max_sitemap_entries => sitemapindex
-            if (not self.allow_multifile):
-                raise Exception("Too many entries for a single sitemap but multifile disabled")
-            # Work out how to name the sitemaps, attempt to add %05d before ".xml$", else append
-            sitemap_prefix = basename
-            sitemap_suffix = '.xml'
-            if (basename[-4:] == '.xml'):
-                sitemap_prefix = basename[:-4]
-            # Use iterator over all resources and count off sets of
-            # max_sitemap_entries to go into each sitemap, store the
-            # names of the sitemaps as we go
-            sitemaps={}
-            while (len(chunk)>0):
-                file = sitemap_prefix + ( "%05d" % (len(sitemaps)) ) + sitemap_suffix
-                self.logger.info("Writing sitemap %s..." % (file))
-                f = open(file, 'w')
-                f.write(self.resources_as_xml(chunk))
-                f.close()
-                # Record timestamp
-                sitemaps[file] = os.stat(file).st_mtime
-                # Get next chunk
-                ( chunk, next ) = self.get_resources_chunk(resources_iter,next)
-            self.logger.info("Wrote %d sitemaps" % (len(sitemaps)))
-            f = open(basename, 'w')
-            self.logger.info("Writing sitemapindex %s..." % (basename))
-            f.write(self.sitemapindex_as_xml(sitemaps=sitemaps,resource_list=resources))
-            f.close()
-            self.logger.info("Wrote sitemapindex %s" % (basename))
-        else:
-            f = open(basename, 'w')
-            self.logger.info("Writing sitemap %s..." % (basename))
-            f.write(self.resources_as_xml(chunk))
-            f.close()
-            self.logger.info("Wrote sitemap %s" % (basename))
-
-    def get_resources_chunk(self, resource_iter, first=None):
-        """Return next chunk of resources from resource_iter, and next item
+    def resources_as_xml(self, resources, num_resources=None, sitemapindex=False, fh=None):
+        """Return XML for a set of resources in sitemap format
         
-        If first parameter is specified then this will be prepended to
-        the list.
+        Arguments:
+        - resources - either an iterable or iterator of Resource objects.
+        - num_resources - if not None then only that number will be written
+        - sitemapindex - set True to write sitemapindex instead of sitemap
+        - fh - write to filehandle fh instead of returning string
 
-        The chunk will contain self.max_sitemap_entries if the iterator 
-        returns that many. next will have the value of the next value from
-        the iterator, providing indication of whether more is available. 
-        Use this as first when asking for the following chunk.
         """
-        chunk = []
-        next = None
-        if (first is not None):
-            chunk.append(first)
-        for r in resource_iter:
-            chunk.append(r)
-            if (len(chunk)>self.max_sitemap_entries):
-                break
-        if (len(chunk)>self.max_sitemap_entries):
-            next = chunk.pop()
-        return(chunk,next)
+        # element names depending on sitemapindex or not
+        root_element = ( 'sitemapindex' if (sitemapindex) else 'urlset' )
+        item_element = ( 'sitemap' if (sitemapindex) else 'url')
+        # namespaces and other settings
+        namespaces = { 'xmlns': SITEMAP_NS, 'xmlns:rs': RS_NS }
+        root = Element(root_element, namespaces)
+        if (self.pretty_xml):
+            root.text="\n"
+        # <rs:md>
+        if (hasattr(resources,'md')):
+            self.add_md_to_etree(root,resources.md)
+        # <rs:ln>
+        if (hasattr(resources,'ln')):
+            for ln in resources.ln:
+                self.add_ln_to_etree(root,ln)
+        # <url> entries from either an iterable or an iterator
+        for r in resources:
+            e=self.resource_etree_element(r, element_name=item_element)
+            root.append(e)
+            if (num_resources is not None):
+                num_resources-=1
+                if (num_resources==0):
+                    break
+        # have tree, now serialize
+        tree = ElementTree(root);
+        xml_buf=None
+        if (fh is None):
+            xml_buf=StringIO.StringIO()
+            fh=xml_buf
+        if (sys.version_info < (2,7)):
+            tree.write(xml_buf,encoding='UTF-8')
+        else:
+            tree.write(xml_buf,encoding='UTF-8',xml_declaration=True,method='xml')
+        if (xml_buf is not None):
+            return(xml_buf.getvalue())
+
+
+    ##### Read/parse an XML sitemap or sitemapindex #####
+
+    def parse_xml(self, fh=None, etree=None, resources=None, capability=None, sitemapindex=None):
+        """Parse XML Sitemap from fh or etree and add resources to a
+        resorces object (which must support the add method). Returns 
+        the resources object.
+
+        Also sets self.resources_created to be the number of resources created. 
+        We adopt a very lax approach here. The parsing is properly namespace 
+        aware but we search just for the elements wanted and leave everything 
+        else alone.
+
+        This method will read either sitemap or sitemapindex documents. Behavior
+        depends on the sitemapindex parameter:
+        - None - will read either
+        - False - SitemapIndexError exception if sitemapindex detected
+        - True - SitemapIndexError exception if sitemap detected
+
+        Will set self.parsed_index based on whether a sitemap or sitemapindex 
+        document was read:
+        - False - sitemap
+        - True - sitemapindex
+        """
+        if (resources is None):
+            resources=ResourceContainer()
+        if (fh is not None):
+            etree=parse(fh)
+        elif (etree is None):
+            raise ValueError("Neither fh or etree set")
+        # check root element: urlset (for sitemap), sitemapindex or bad
+        root_tag = etree.getroot().tag
+        resource_tag = None # will be <url> or <sitemap> depending on type
+        self.parsed_index = None
+        if (root_tag == '{'+SITEMAP_NS+"}urlset"):
+            self.parsed_index = False
+            if (sitemapindex is not None and sitemapindex):
+                raise SitemapIndexError("Got sitemap when expecting sitemapindex",etree)
+            resource_tag = '{'+SITEMAP_NS+"}url"
+        elif (root_tag == '{'+SITEMAP_NS+"}sitemapindex"):
+            self.parsed_index = True
+            if (sitemapindex is not None and not sitemapindex):
+                raise SitemapIndexError("Got sitemapindex when expecting sitemap",etree)
+            resource_tag = '{'+SITEMAP_NS+"}sitemap"
+        else:
+            raise ValueError("XML is not sitemap or sitemapindex")
+        
+        # have what we expect, read it
+        in_preamble = True
+        self.resources_created=0
+        for e in etree.getroot().getchildren():
+            # look for <rs:md> and <rs:ln>, first <url> ends
+            # then look for resources in <url> blocks
+            if (e.tag == resource_tag):
+                in_preamble = False #any later rs:md or rs:ln is error
+                r = self.resource_from_etree(e, self.resource_class)
+                try:
+                    resources.add( r )
+                except SitemapDupeError:
+                    self.logger.warning("dupe of: %s (lastmod=%s)" % (r.uri,r.lastmod))
+                self.resources_created+=1
+            elif (e.tag == "{"+RS_NS+"}md"):
+                if (in_preamble):
+                    resources.md = self.md_from_etree(e)
+                else:
+                    raise Exception("Found <rs:md> after first <url> in sitemap")
+            elif (e.tag == "{"+RS_NS+"}ln"):
+                if (in_preamble):
+                    resources.ln.append(self.ln_from_etree(e))
+                else:
+                    raise Exception("Found <rs:md> after first <url> in sitemap")
+            else:
+                # element we don't recognize, ignore
+                # FIXME - might add check for debug?
+                pass
+        # check that we read to right capability document
+        if (capability is not None):
+            if ('capability' not in resources.md):
+                self.logger.warning('No capabiliy specified, assuming resourcelist')
+                resources.md['capability'] = 'resourcelist'
+            elif (resources.md['capability'] != capability):
+                raise ValueError("Expected to read a %s document, got %s" %
+                                 (capability,resources.md['capability']))
+        # return the resource container object
+        return(resources)
+
 
     ##### Resource methods #####
 
@@ -307,155 +364,6 @@ class Sitemap(object):
                 raise Exception("Invalid length element in <rs:ln> for %s" % (context))
         return(ln)
 
-    ##### ResourceContainer (ResourceList or Changelist) methods #####
-
-    def resources_as_xml(self, resources, num_resources=None, sitemapindex=False):
-        """Return XML for a set of resources in sitemap format
-        
-        resources is either an iterable or iterator of Resource objects.
-
-        If num_resources is not None then only that number will be written
-        before returning.
-        """
-        # element names depending on sitemapindex or not
-        root_element = ( 'sitemapindex' if (sitemapindex) else 'urlset' )
-        item_element = ( 'sitemap' if (sitemapindex) else 'url')
-        # namespaces and other settings
-        namespaces = { 'xmlns': SITEMAP_NS, 'xmlns:rs': RS_NS }
-        root = Element(root_element, namespaces)
-        if (self.pretty_xml):
-            root.text="\n"
-        # <rs:md>
-        if (hasattr(resources,'md')):
-            self.add_md_to_etree(root,resources.md)
-        # <rs:ln>
-        if (hasattr(resources,'ln')):
-            for ln in resources.ln:
-                self.add_ln_to_etree(root,ln)
-        # <url> entries from either an iterable or an iterator
-        for r in resources:
-            e=self.resource_etree_element(r, element_name=item_element)
-            root.append(e)
-            if (num_resources is not None):
-                num_resources-=1
-                if (num_resources==0):
-                    break
-        # have tree, now serialize
-        tree = ElementTree(root);
-        xml_buf=StringIO.StringIO()
-        if (sys.version_info < (2,7)):
-            tree.write(xml_buf,encoding='UTF-8')
-        else:
-            tree.write(xml_buf,encoding='UTF-8',xml_declaration=True,method='xml')
-        return(xml_buf.getvalue())
-
-    def parse_xml(self, fh=None, etree=None, resources=None, capability=None, sitemapindex=None):
-        """Parse XML Sitemap from fh or etree and add resources to a
-        resorces object (which must support the add method). Returns 
-        the resources object.
-
-        Also sets self.resources_created to be the number of resources created. 
-        We adopt a very lax approach here. The parsing is properly namespace 
-        aware but we search just for the elements wanted and leave everything 
-        else alone.
-
-        This method will read either sitemap or sitemapindex documents. Behavior
-        depends on the sitemapindex parameter:
-        - None - will read either
-        - False - SitemapIndexError exception if sitemapindex detected
-        - True - SitemapIndexError exception if sitemap detected
-
-        Will set self.parsed_index based on whether a sitemap or sitemapindex 
-        document was read:
-        - False - sitemap
-        - True - sitemapindex
-        """
-        if (resources is None):
-            resources=ResourceContainer()
-        if (fh is not None):
-            etree=parse(fh)
-        elif (etree is None):
-            raise ValueError("Neither fh or etree set")
-        # check root element: urlset (for sitemap), sitemapindex or bad
-        root_tag = etree.getroot().tag
-        resource_tag = None # will be <url> or <sitemap> depending on type
-        self.parsed_index = None
-        if (root_tag == '{'+SITEMAP_NS+"}urlset"):
-            self.parsed_index = False
-            if (sitemapindex is not None and sitemapindex):
-                raise SitemapIndexError("Got sitemap when expecting sitemapindex",etree)
-            resource_tag = '{'+SITEMAP_NS+"}url"
-        elif (root_tag == '{'+SITEMAP_NS+"}sitemapindex"):
-            self.parsed_index = True
-            if (sitemapindex is not None and not sitemapindex):
-                raise SitemapIndexError("Got sitemapindex when expecting sitemap",etree)
-            resource_tag = '{'+SITEMAP_NS+"}sitemap"
-        else:
-            raise ValueError("XML is not sitemap or sitemapindex")
-        
-        # have what we expect, read it
-        in_preamble = True
-        self.resources_created=0
-        for e in etree.getroot().getchildren():
-            # look for <rs:md> and <rs:ln>, first <url> ends
-            # then look for resources in <url> blocks
-            if (e.tag == resource_tag):
-                in_preamble = False #any later rs:md or rs:ln is error
-                r = self.resource_from_etree(e, self.resource_class)
-                try:
-                    resources.add( r )
-                except SitemapDupeError:
-                    self.logger.warning("dupe of: %s (lastmod=%s)" % (r.uri,r.lastmod))
-                self.resources_created+=1
-            elif (e.tag == "{"+RS_NS+"}md"):
-                if (in_preamble):
-                    resources.md = self.md_from_etree(e)
-                else:
-                    raise Exception("Found <rs:md> after first <url> in sitemap")
-            elif (e.tag == "{"+RS_NS+"}ln"):
-                if (in_preamble):
-                    resources.ln.append(self.ln_from_etree(e))
-                else:
-                    raise Exception("Found <rs:md> after first <url> in sitemap")
-            else:
-                # element we don't recognize, ignore
-                # FIXME - might add check for debug?
-                pass
-        # check that we read to right capability document
-        if (capability is not None):
-            if ('capability' not in resources.md):
-                self.logger.warning('No capabiliy specified, assuming resourcelist')
-                resources.md['capability'] = 'resourcelist'
-            elif (resources.md['capability'] != capability):
-                raise ValueError("Expected to read a %s document, got %s" %
-                                 (capability,resources.md['capability']))
-        # return the resource container object
-        return(resources)
-
-    ##### Sitemap Index #####
-
-    def sitemapindex_as_xml(self, file=None, sitemaps={}):
-        """Return a sitemapindex as an XML string
-
-        Format:
-        <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-          <sitemap>
-            <loc>http://www.example.com/sitemap1.xml.gz</loc>
-            <lastmod>2004-10-01T18:23:17+00:00</lastmod>
-          </sitemap>
-          ...more...
-        </sitemapeindex>
-        """
-        resources = ResourceContainer()
-        for file in sitemaps.keys():
-            try:
-                uri = self.mapper.dst_to_src(file)
-            except MapperError:
-                uri = 'file://'+file
-                self.logger.error("sitemapindex: can't map %s into URI space, writing %s" % (file,uri))
-            # Make a Resource for the Sitemap and serialize
-            resources.add( Resource( uri=uri, timestamp=sitemaps[file] ) )
-        return( self.resources_as_xml(resources, sitemapindex=True))
 
     ##### Metadata and links #####
 
