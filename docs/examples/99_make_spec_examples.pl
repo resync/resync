9e8c4bcd3917b766f893c8b95d9f2457a102ad40
Simeon Warner <simeon.warner@cornell.edu>
2013-02-27 22:56:38 -0500

    Add section 4 and section 5 examples

--- docs/examples/99_make_spec_examples.pl
+++ docs/examples/99_make_spec_examples.pl
@@ -99,4 +99,92 @@ rl.add( Resource("http://example.com/res2",
 		 sha256="854f61290e2e197a11bc91063afce22e43f8ccc655237050ace766adc68dc784",
                  length="14599",
                  type="application/pdf") )
+print rl.as_xml(pretty_xml=True)
+
+head("Example 4.2: A Resource List Index")
+rl = ResourceList()
+rl.modified="2013-01-03T09:00:00Z"
+rl.ln.append({"rel":"resourcesync",
+              "href":"http://example.com/dataset1/capabilitylist.xml"})
+rl.add( Resource("http://example.com/resourcelist3.xml",
+                 lastmod="2013-01-03T09:00:00Z") )
+rl.add( Resource("http://example.com/resourcelist2.xml",
+                 lastmod="2013-01-03T09:00:00Z") )
+rl.add( Resource("http://example.com/resourcelist1.xml",
+                 lastmod="2013-01-03T09:00:00Z") )
 print rl.index_as_xml(pretty_xml=True)
+
+head("Example 4.3: A Resource List with a navigational link to its parent Resource List Index")
+rl = ResourceList()
+rl.modified="2013-01-03T09:00:00Z"
+rl.ln.append({"rel":"resourcesync",
+              "href":"http://example.com/dataset1/capabilitylist.xml"})
+rl.ln.append({"rel":"up",
+              "href":"http://example.com/dataset1/resourcelist-index.xml"})
+rl.add( Resource("http://example.com/res3",
+                 lastmod="2013-01-03T09:00:00Z",
+                 md5="1584abdf8ebdc9802ac0c6a7402c8753",
+                 length="4385",
+                 type="application/pdf") )
+rl.add( Resource("http://example.com/res2",
+                 lastmod="2013-01-03T09:00:00Z",
+                 md5="4556abdf8ebdc9802ac0c6a7402c9881",
+                 length="883",
+                 type="image/png") )
+print rl.as_xml(pretty_xml=True)
+
+head("Example 5.1: A Resource Dump document")
+rd = ResourceDump()
+rd.modified = "2013-01-03T09:00:00Z"
+rd.ln.append({"rel":"resourcesync",
+              "href":"http://example.com/dataset1/capabilitylist.xml"})
+rd.add( Resource("http://example.com/resourcedump-part3.zip",
+		 lastmod="2013-01-03T09:00:00Z",
+		 type="application/zip",
+		 length="4765") )
+rd.add( Resource("http://example.com/resourcedump-part2.zip",
+		 lastmod="2013-01-03T09:00:00Z",
+                 type="application/zip",
+		 length="9875") )
+rd.add( Resource("http://example.com/resourcedump-part1.zip",
+                 lastmod="2013-01-03T09:00:00Z",
+                 type="application/zip",
+                 length="2298") )
+print rd.as_xml(pretty_xml=True)
+
+head("Example 5.2: A Resource Dump Manifest")
+rdm = ResourceDumpManifest()
+rdm.modified="2013-01-03T09:00:00Z"
+rdm.ln.append({"rel":"resourcesync",
+               "href":"http://example.com/dataset1/capabilitylist.xml"})
+rdm.add( Resource("http://example.com/res1",
+		  lastmod="2013-01-02T13:00:00Z",
+		  md5="1584abdf8ebdc9802ac0c6a7402c03b6",
+		  length="8876",
+		  type="text/html",
+		  path="/resources/res1") )
+rdm.add( Resource("http://example.com/res2",
+		  lastmod="2013-01-02T14:00:00Z",
+                  md5="1e0d5cb8ef6ba40c99b14c0237be735e",
+                  sha256="854f61290e2e197a11bc91063afce22e43f8ccc655237050ace766adc68dc784",
+		  length="14599",
+		  type="application/pdf",
+		  path="/resources/res2") )
+print rdm.as_xml(pretty_xml=True)
+
+head("Example 5.3: A Resource Dump Manifest Index")
+rdm = ResourceDumpManifest()
+rdm.modified="2013-01-03T09:00:00Z"
+rdm.ln.append({"rel":"resourcesync",
+               "href":"http://example.com/dataset1/capabilitylist.xml"})
+rdm.add( Resource("/manifests/part3.xml",
+		  lastmod="2013-01-03T09:00:00Z",
+                  path="/manifests/part3.xml") )
+rdm.add( Resource("/manifests/part2.xml",
+		  lastmod="2013-01-03T09:00:00Z",
+		  path="/manifests/part2.xml") )
+rdm.add( Resource("/manifests/part1.xml",
+		  lastmod="2013-01-03T09:00:00Z",
+		  path="/manifests/part1.xml") )
+print rdm.index_as_xml(pretty_xml=True)
+
