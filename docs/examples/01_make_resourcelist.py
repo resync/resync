22472d7947c16388dc849b2c317ee4d509322754
Simeon Warner <simeon.warner@cornell.edu>
2013-02-27 21:21:56 -0500

    Update to use ResourceList methods

--- docs/examples/01_make_resourcelist.py
+++ docs/examples/01_make_resourcelist.py
@@ -5,5 +5,4 @@ from resync.sitemap import Sitemap
 rl = ResourceList()
 rl.add( Resource('http://example.com/res1', lastmod='2013-01-01') )
 rl.add( Resource('http://example.com/res2', lastmod='2013-01-02') )
-sm = Sitemap(pretty_xml=True)
-print sm.resources_as_xml(rl)
+print rl.as_xml(pretty_xml=True)
