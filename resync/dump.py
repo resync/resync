371905290da2b8af0c938f994551c82ff90ec8f0
Simeon Warner <simeon.warner@cornell.edu>
2013-05-28 18:37:00 -0400

    Use shorter paths in dump be removing common prefix

--- resync/dump.py
+++ resync/dump.py
@@ -8,7 +8,7 @@ class DumpError(Exception):
     pass
 
 class Dump(object):
-    """Dump of resource content associated with an resource_list or change set
+    """Dump of content for a Resource Dump or Change Dump
 
     The resource_list must be comprised of Resource objects
     which have the path attributes set to indicate the local
@@ -20,9 +20,10 @@ class Dump(object):
         self.compress = compress
         self.max_size = 100*1024*1024 #100MB
         self.max_files = 50000
+        self.path_prefix = None
         
     def write(self, resource_list=None, dumpfile=None):
-        """Write a dump file"""
+        """Wrapper to Write a dump file"""
         self.check_files(resource_list)
         if (self.format == 'zip'):
             self.write_zip(resource_list,dumpfile)
@@ -32,15 +33,20 @@ class Dump(object):
             raise DumpError("Unknown dump format '%s'" % (self.format))
 
     def write_zip(self, resource_list=None, dumpfile=None):
-        """Write a ZIP dump file"""
+        """Write a ZIP format dump file"""
         compression = ( ZIP_DEFLATED if self.compress else ZIP_STORED )
         zf = ZipFile(dumpfile, mode="w", compression=compression, allowZip64=True)
         # Write resource_list first
-        s = Sitemap(pretty_xml=True, allow_multifile=False)
+        s = Sitemap()
+        real_path = {}
+        for resource in resource_list:
+            archive_path = self.archive_path(resource.path)
+            real_path[archive_path] = resource.path
+            resource.path = archive_path
         zf.writestr('manifest.xml',s.resources_as_xml(resource_list))
         # Add all files in the resource_list
         for resource in resource_list:
-            zf.write(resource.uri)
+            zf.write(real_path[resource.path], arcname=resource.path)
         zf.close()
         zipsize = os.path.getsize(dumpfile)
         print "Wrote ZIP file dump %s with size %d bytes" % (dumpfile,zipsize)
@@ -63,23 +69,44 @@ class Dump(object):
             wh.content_type = 'text/plain'
             wh.result_code = 200
             wh.checksum = 'aabbcc'
-            wh.location = 'loc'
+            wh.location = self.archive_path(resource.path)
             wf.write_record( WARCRecord( header=wh, payload=resource.path ) )
         wf.close()
         warcsize = os.path.getsize(dumpfile)
         print "Wrote WARC file dump %s with size %d bytes" % (dumpfile,warcsize)
         
     def check_files(self,resource_list):
-        """Go though and check all files in resource_list, add up size"""
+        """Go though and check all files in resource_list, add up size, find common path
+
+        Will find the longest common path prefix that can be used when writing the
+        dump file. Saved in self.path_prefix.
+        """
         if (len(resource_list) > self.max_files):
             raise DumpError("Number of files to dump (%d) exceeds maximum (%d)" % (len(resource_list),self.max_files))
         total_size = 0 #total size of all files in bytes
+        path_prefix = None
         for resource in resource_list:
             if (resource.path is None):
                 #explicit test because exception raised by getsize otherwise confusing
                 raise DumpError("No file path defined for resource %s" % resource.uri)
+            if (path_prefix is None):
+                path_prefix = os.path.dirname(resource.path)
+            else:
+                path_prefix = os.path.commonprefix( [ path_prefix, os.path.dirname(resource.path) ])
             total_size += os.path.getsize(resource.path)
+        self.path_prefix = path_prefix
         self.total_size = total_size
         print "Total size of files to include in dump %d bytes" % (total_size)
         if (total_size > self.max_size):
             raise DumpError("Size of files to dump (%d) exceeds maximum (%d)" % (total_size,self.max_size))
+
+    def archive_path(self,real_path):
+        """Return the archive path for file with real_path
+
+        Mapping is based on removal of self.path_prefix which is determined
+        by self.check_files().
+        """
+        if (not self.path_prefix):
+            return(real_path)
+        else:
+            return( os.path.relpath(real_path,self.path_prefix) )
