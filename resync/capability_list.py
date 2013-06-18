905f185cf45db97cfd03efd08232c9781e21beaf
Simeon Warner <simeon.warner@cornell.edu>
2013-06-17 20:07:19 -0400

    Add resources class with ordering by capability

--- resync/capability_list.py
+++ resync/capability_list.py
@@ -1,9 +1,8 @@
-"""ResourceSync capability_list object
+"""ResourceSync Capability List object
 
-An capability_list is a set of capabilitys with some metadata for each 
-capability. 
-
-The capability_list object may also contain metadata and links.
+An Capability List is a set of capabilitys with some metadata for 
+each capability. The Capability List object may also contain metadata 
+and links like other lists.
 """
 
 import collections
@@ -13,26 +12,65 @@ from resource_set import ResourceSet
 from list_base import ListBase
 from sitemap import Sitemap
 
+class CapabilitySet(ResourceSet):
+    """Class for storage of resources in a Capability List
+
+    Extends the ResourceSet to add checks to ensure that there are 
+    never two entries for the same resource, and that values are 
+    returned in the canonical order.
+    """
+
+    def __init__(self):
+        self.order = [ 'resourcelist', 'resourcedump',
+                       'changelist', 'changedump',
+                       'resourcelist-archive', 'resourcedump-archive',
+                       'changelist-archive', 'changedump-archive' ]
+
+    def __iter__(self):
+        """Iterator over all the resources in capability order
+
+        Deals with the case of unknown capabilities or duplicate entries
+        by using uri order for duplicates and adding any unknown ones 
+        at the end
+        """
+        self._iter_next_list = []
+        # look through all resources and build capability to uri index
+        uris = {}
+        for uri in self.keys():
+            cap = self[uri].capability
+            if (cap not in uris):
+                uris[cap]=[]
+            uris[cap].append(uri)
+        # now build list or uris in order for iterator
+        for cap in uris:
+            for uri in sorted(uris[cap]):
+                self._iter_next_list.append(uri)
+        self._iter_next_list.reverse()
+        return(iter(self._iter_next, None))
+
+    def _iter_next(self):
+        if (len(self._iter_next_list)>0):
+            return(self[self._iter_next_list.pop()])
+        else:
+            return(None)
+
+
 class CapabilityList(ListBase):
-    """Class representing an capability_list of resources
+    """Class representing a Capability List
 
-    This same class is used for both the source and the destination
-    and is the central point of comparison the decide whether they
-    are in sync or what needs to be copied to bring the destinaton
-    into sync.
-
-    An capability_list will admit only one resource with any given URI.
-
-    Storage is unordered but the iterator imposes a canonical order
-    which is currently alphabetical by URI.
+    An Capability List will admit only one resource with any given 
+    URI. The iterator over resources is expected to return them in
+    canonical order of capability names as defined in main specification
+    section 9.2 and archives specification section 6.
     """
 
     def __init__(self, resources=None, md=None, ln=None):
         if (resources is None):
-            resources = ResourceSet()
+            resources = CapabilitySet()
         super(CapabilityList, self).__init__(resources=resources, md=md, ln=ln)
         self.capability_name='capabilitylist'
         self.capability_md='capabilitylist'
+        self.md['from']=None #usually don't want a from date
 
     def add(self, resource, replace=False):
         """Add a resource or an iterable collection of resources
