7c51e8640ac5b3e3f3ef18fa0688c170cd56b18e
Rob Sanderson <azaroth42@gmail.com>
2013-06-27 16:08:04 -0600

    Adding useful classes to __init__

--- resync/__init__.py
+++ resync/__init__.py
@@ -1,1 +1,9 @@
 from _version import __version__
+
+from resource_list import ResourceList
+from change_list import ChangeList
+from capability_list import CapabilityList
+from resource_dump import ResourceDump 
+from resource import Resource 
+
+
