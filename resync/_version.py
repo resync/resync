0f295012502ed6b8b599d8645b4c91564edd4175
Rob Sanderson <azaroth42@gmail.com>
2013-06-27 16:49:47 -0600

    Added checking for change value, and its presence when adding to ChangeList

--- resync/_version.py
+++ resync/_version.py
@@ -4,3 +4,6 @@
 # x.y is spec version, see http://www.openarchives.org/rs/x.y/
 # z is incremented for revisions within that version, 1...
 __version__ = '0.6.2'
+
+# None is to allow the resource not be a change
+CHANGE_TYPES = ['created', 'updated', 'deleted', None] 
