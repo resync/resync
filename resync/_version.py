06f250d3143e7d0e98733b2fbbb90a5256830639
Simeon Warner <simeon.warner@cornell.edu>
2013-09-06 08:43:56 -0400

    Bump version to 0.9.4

--- resync/_version.py
+++ resync/_version.py
@@ -3,4 +3,4 @@
 # Format: x.y.z where
 # x.y is spec version, see http://www.openarchives.org/rs/x.y/
 # z is incremented for revisions within that version, 1...
-__version__ = '0.9.3'
+__version__ = '0.9.4'
