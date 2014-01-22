58daf6f2225cdf52079072eee47f23a6d188cfa9
Simeon Warner <simeon.warner@cornell.edu>
2014-01-21 19:20:31 -0500

    Change comment to indicate choice of alphanum order by uri

--- resync/resource_set.py
+++ resync/resource_set.py
@@ -1,7 +1,8 @@
 """A set of Resource objects used for Capability List
 Indexes and ResourceSync Description documents.
 
-FIXME - what should the ordering be?
+Ordinging is currently alphanumeric (using sorted(..)) on the
+uri which is the key. 
 """
 
 class ResourceSet(dict):
