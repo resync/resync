adaee4d5a2f53cae6900870b761d0919b2c8ac4d
Simeon Warner <simeon.warner@cornell.edu>
2014-06-18 18:33:02 +0300

    Add ChangeDumpManifest skeleton like ResourceDumpManifest

--- resync/__init__.py
+++ resync/__init__.py
@@ -8,7 +8,9 @@ from capability_list import CapabilityList
 from resource_list import ResourceList
 from change_list import ChangeList
 from resource_dump import ResourceDump
+from resource_dump_manifest import ResourceDumpManifest
 from change_dump import ChangeDump
+from resync.change_dump import ChangeDump
 from archives import ResourceListArchive,ResourceDumpArchive,ChangeListArchive,ChangeDumpArchive
 from resource import Resource
 
---
+++ resync/change_dump_manifest.py
@@ -0,0 +1,27 @@
+"""ResourceSync ChangeDumpManifest object
+
+A ChangeDumpManifest lists the set of files/resources included 
+within a content package that is included in a ChangeDump. 
+
+The ChangeeDumpManifest object will include the change type,
+a path for each item (except in the case of "deleted"), and may 
+also contain metadata and links.
+
+Described in specification at:
+http://www.openarchives.org/rs/resourcesync#ChangeDumpManifest
+"""
+
+from change_list import ChangeList
+
+class ChangeDumpManifest(ChangeList):
+    """Class representing a Change Dump Manifest
+
+    A ChangeDumpManifest comprises a set of files/resources
+    in a content package. Properties much Like a ChangeList
+    and implemented as a sub-class of ChangeeList.
+    """
+
+    def __init__(self, resources=None, md=None, ln=None, uri=None, allow_multifile=None, mapper=None):
+        super(ChangeDumpManifest, self).__init__(resources=resources, md=md, ln=ln, uri=uri, mapper=mapper)
+        self.capability_name = 'changedump-manifest'
+
