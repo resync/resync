48b7f6a6b0228c7b178261137f1a977101856ea2
Simeon Warner <simeon.warner@cornell.edu>
2016-05-06 22:29:55 -0400

    Test coverage, pep8

--- resync/mapper.py
+++ resync/mapper.py
@@ -3,11 +3,12 @@
 import os
 import os.path
 import re
-try: #python3
+try:  # python3
     from urllib.parse import urlparse
-except ImportError: #python2
+except ImportError:  # python2
     from urlparse import urlparse
 import logging
+
 
 class Mapper():
     """Mapper object to map between source URIs and destination paths.
@@ -18,7 +19,7 @@ class Mapper():
     def __init__(self, mappings=None, use_default_path=False):
         """Initialize Mapper."""
         self.logger = logging.getLogger('resync.mapper')
-        self.mappings=[]
+        self.mappings = []
         if (mappings):
             self.parse(mappings, use_default_path)
 
@@ -30,39 +31,49 @@ class Mapper():
         """Parse a list of map strings (mappings).
 
         Accepts two distinct formats:
-        1. If there are exactly two entries then these may be the source base URI
-        and the destination base path. Neither may contain an equals (=) sign.
-        2. For any number of mapping stings interpret each as a mapping URI=path. 
-        These are in the order they will be tried.
-        
+        1. If there are exactly two entries then these may be the source
+        base URI and the destination base path. Neither may contain an
+        equals (=) sign.
+        2. For any number of mapping stings interpret each as a mapping
+        URI=path. These are in the order they will be tried.
+
         And if use_default_path is True then a third:
-        3. If there is exactly one entry and it does not contain an equals (=)
-        sign then a safe local path is created based on the source base URI. In
-        the case that the source base URI is a local path already then an indentity 
-        mapping is used.
+        3. If there is exactly one entry and it does not contain an
+        equals (=) sign then a safe local path is created based on the
+        source base URI. In the case that the source base URI is a local
+        path already then an indentity mapping is used.
         """
         if (use_default_path and
-            len(mappings)==1 and
-            re.search(r"=",mappings[0])==None):
+                len(mappings) == 1 and
+                re.search(r"=", mappings[0]) is None):
             path = self.path_from_uri(mappings[0])
-            self.logger.warning("Using URI mapping: %s -> %s" % (mappings[0],path))
-            self.mappings.append(Map(mappings[0],path))
-        elif (len(mappings)==2 and 
-            re.search(r"=",mappings[0])==None and 
-            re.search(r"=",mappings[1])==None):
-            self.mappings.append(Map(mappings[0],mappings[1]))
+            self.logger.warning("Using URI mapping: %s -> %s" %
+                                (mappings[0], path))
+            self.mappings.append(Map(mappings[0], path))
+        elif (len(mappings) == 2 and
+              re.search(r"=", mappings[0]) is None and
+              re.search(r"=", mappings[1]) is None):
+            self.mappings.append(Map(mappings[0], mappings[1]))
         else:
             for mapping in mappings:
-                l=mapping.split('=')
-                if (len(l)!=2):
-                    raise MapperError("Bad mapping argument (%s), got %s"%(mapping,str(l)))
-                (src_uri,dst_path)=l
+                l = mapping.split('=')
+                if (len(l) != 2):
+                    raise MapperError(
+                        "Bad mapping argument (%s), got %s" %
+                        (mapping, str(l)))
+                (src_uri, dst_path) = l
                 # Check for dupes
                 for map in self.mappings:
                     if (src_uri == map.src_uri):
-                        raise MapperError("Attempt to set duplicate mapping for source URI %s (with destination path %s)" % (src_uri,dst_path))
+                        raise MapperError(
+                            "Attempt to set duplicate mapping for "
+                            "source URI %s (with destination path %s)" %
+                            (src_uri, dst_path))
                     if (dst_path == map.dst_path):
-                        raise MapperError("Attempt to set duplicate mapping for destination path %s (with source URI %s)" % (dst_path,src_uri))
+                        raise MapperError(
+                            "Attempt to set duplicate mapping for "
+                            "destination path %s (with source URI %s)" %
+                            (dst_path, src_uri))
                 self.mappings.append(Map(src_uri, dst_path))
 
     def default_src_uri(self):
@@ -70,15 +81,15 @@ class Mapper():
 
         This is take just to be the src_uri of the first entry
         """
-        if (len(self.mappings)>0):
+        if (len(self.mappings) > 0):
             return(self.mappings[0].src_uri)
         raise MapperError("Can't get default source URI from empty mapping")
 
     def unsafe(self):
-        """True is one or more mapping is unsafe.
+        """True if one or more mapping is unsafe.
 
-        See Map.unsafe() for logic. Provide this as a test rather than 
-        building into object creation/parse because it is useful to 
+        See Map.unsafe() for logic. Provide this as a test rather than
+        building into object creation/parse because it is useful to
         allow unsafe mappings in situations where it doesn't matter.
         """
         for map in self.mappings:
@@ -86,34 +97,38 @@ class Mapper():
                 return(True)
         return(False)
 
-    def dst_to_src(self,dst_file):
+    def dst_to_src(self, dst_file):
         """Map destination path to source URI."""
         for map in self.mappings:
             src_uri = map.dst_to_src(dst_file)
             if (src_uri is not None):
                 return(src_uri)
         # Must have failed if loop exited
-        raise MapperError("Unable to translate destination path (%s) into a source URI." % (dst_file))
+        raise MapperError(
+            "Unable to translate destination path (%s) "
+            "into a source URI." % (dst_file))
 
-    def src_to_dst(self,src_uri):
+    def src_to_dst(self, src_uri):
         """Map source URI to destination path."""
         for map in self.mappings:
             dst_path = map.src_to_dst(src_uri)
             if (dst_path is not None):
                 return(dst_path)
         # Must have failed if loop exited
-        raise MapperError("Unable to translate source URI (%s) into a destination path." % (src_uri))
+        raise MapperError(
+            "Unable to translate source URI (%s) into "
+            "a destination path." % (src_uri))
 
-    def path_from_uri(self,uri):
+    def path_from_uri(self, uri):
         """Make a safe path name from uri.
 
-        In the case that uri is already a local path then the same path
-        is returned.
+        In the case that uri is already a local path then the
+        same path is returned.
         """
-        (scheme, netloc, path, params, query, fragment) = urlparse( uri )
+        (scheme, netloc, path, params, query, frag) = urlparse(uri)
         if (netloc == ''):
             return(uri)
-        path = '/'.join([netloc,path])
+        path = '/'.join([netloc, path])
         path = re.sub('[^\w\-\.]', '_', path)
         path = re.sub('__+', '_', path)
         path = re.sub('[_\.]+$', '', path)
@@ -133,58 +148,61 @@ class Map:
 
     Both URI and destination paths are assumed to use / as the path
     separator. No account is take for other path separators used
-    for paths on non-unix systems. This translation must be done 
+    for paths on non-unix systems. This translation must be done
     elsewhere by consideration of os.sep.
     """
 
-    def __init__(self,src_uri=None,dst_path=None):
-        """Initialize Map object, strip trailing slashes from src_uri and dst_path."""
-        self.src_uri  = self.strip_trailing_slashes(src_uri)
+    def __init__(self, src_uri=None, dst_path=None):
+        """Initialize Map object.
+
+        Will strip trailing slashes from src_uri and dst_path.
+        """
+        self.src_uri = self.strip_trailing_slashes(src_uri)
         self.dst_path = self.strip_trailing_slashes(dst_path)
 
-    def strip_trailing_slashes(self,path):
+    def strip_trailing_slashes(self, path):
         """Return input path minus any trailing slashes."""
-        m=re.match(r"(.*)/+$",path)
+        m = re.match(r"(.*)/+$", path)
         if (m is None):
             return(path)
         return(m.group(1))
 
-    def dst_to_src(self,dst_file):
+    def dst_to_src(self, dst_file):
         """Return the src URI from the dst filepath.
 
-        This does not rely on the destination filepath actually existing on the local
-        filesystem, just on pattern matching. Return source URI on success, None on 
-        failure.
+        This does not rely on the destination filepath actually
+        existing on the local filesystem, just on pattern matching.
+        Return source URI on success, None on failure.
         """
-        m=re.match(self.dst_path+"/(.*)$",dst_file)
+        m = re.match(self.dst_path + "/(.*)$", dst_file)
         if (m is None):
             return(None)
-        rel_path=m.group(1)
-        return(self.src_uri+'/'+rel_path)
-        
-    def src_to_dst(self,src_uri):
+        rel_path = m.group(1)
+        return(self.src_uri + '/' + rel_path)
+
+    def src_to_dst(self, src_uri):
         """Return the dst filepath from the src URI.
-        
+
         Returns None on failure, destination path on success.
         """
-        m=re.match(self.src_uri+"/(.*)$",src_uri)
+        m = re.match(self.src_uri + "/(.*)$", src_uri)
         if (m is None):
             return(None)
-        rel_path=m.group(1)
-        return(self.dst_path+'/'+rel_path)
+        rel_path = m.group(1)
+        return(self.dst_path + '/' + rel_path)
 
     def unsafe(self):
-        """True is the mapping is unsafe for an update.
+        """True if the mapping is unsafe for an update.
 
-        Applies only to local source. Returns True if the paths for source and 
+        Applies only to local source. Returns True if the paths for source and
         destination are the same, or if one is a component of the other path.
         """
-        (scheme, netloc, path, params, query, fragment) = urlparse( self.src_uri )
+        (scheme, netloc, path, params, query, frag) = urlparse(self.src_uri)
         if (scheme != ''):
             return(False)
         s = os.path.normpath(self.src_uri)
         d = os.path.normpath(self.dst_path)
-        lcp = os.path.commonprefix([s,d])
+        lcp = os.path.commonprefix([s, d])
         return(s == lcp or d == lcp)
 
     def __repr__(self):
--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -1,95 +1,115 @@
+"""Tests for resync.mapper."""
 import unittest
 from resync.mapper import Mapper, MapperError, Map
+
 
 class TestMapper(unittest.TestCase):
 
     def test00_mapper_creation(self):
-        m1=Mapper( ['http://e.org/p/','/tmp/q/'] )
-        self.assertEqual( len(m1), 1 )
-        m2=Mapper( mappings=['http://e.org/p','/tmp/q'] )
-        self.assertEqual( len(m2), 1 )
-        self.assertEqual( str(m1), str(m2) )
-        m3=Mapper( ['http://e.org/p/=/tmp/q/'] )
-        self.assertEqual( len(m3), 1 )
-        self.assertEqual( str(m1), str(m3) )
-        m4=Mapper( ['http://e.org/p/=/tmp/q/','http://e.org/r/=/tmp/s/'] )
-        m5=Mapper( ['http://e.org/r/=/tmp/s/','http://e.org/p/=/tmp/q/'] )
-        self.assertEqual( len(m4), 2 )
-        self.assertEqual( len(m5), 2 )
-        self.assertNotEqual( str(m4), str(m5) )
-        
+        m1 = Mapper(['http://e.org/p/', '/tmp/q/'])
+        self.assertEqual(len(m1), 1)
+        m2 = Mapper(mappings=['http://e.org/p', '/tmp/q'])
+        self.assertEqual(len(m2), 1)
+        self.assertEqual(str(m1), str(m2))
+        m3 = Mapper(['http://e.org/p/=/tmp/q/'])
+        self.assertEqual(len(m3), 1)
+        self.assertEqual(str(m1), str(m3))
+        m4 = Mapper(['http://e.org/p/=/tmp/q/', 'http://e.org/r/=/tmp/s/'])
+        m5 = Mapper(['http://e.org/r/=/tmp/s/', 'http://e.org/p/=/tmp/q/'])
+        self.assertEqual(len(m4), 2)
+        self.assertEqual(len(m5), 2)
+        self.assertNotEqual(str(m4), str(m5))
+        # error cases
+        m6 = Mapper()
+        # too many equals
+        self.assertRaises(MapperError, m6.parse, ['a=b=c'])
+        self.assertRaises(MapperError, m6.parse, ['a=b=c=d'])
+        # dupes
+        self.assertRaises(MapperError, m6.parse, ['a=b','a=c'])
+        self.assertRaises(MapperError, m6.parse, ['x=z','y=z'])
 
     def test01_mapper_src_to_dst(self):
-        m=Mapper( ['http://e.org/p/','/tmp/q/'] )
-        self.assertEqual( m.src_to_dst('http://e.org/p/'), '/tmp/q/')
-        self.assertEqual( m.src_to_dst('http://e.org/p/aa'), '/tmp/q/aa')
-        self.assertEqual( m.src_to_dst('http://e.org/p/aa/bb'), '/tmp/q/aa/bb')
-        self.assertEqual( m.src_to_dst('http://e.org/p/aa/bb/'), '/tmp/q/aa/bb/')
-        self.assertRaises( MapperError, m.src_to_dst, 'http://e.org/p' )
-        self.assertRaises( MapperError, m.src_to_dst, 'http://e.org/pa' )
-        self.assertRaises( MapperError, m.src_to_dst, 'nomatch' )
+        m = Mapper(['http://e.org/p/', '/tmp/q/'])
+        self.assertEqual(m.src_to_dst('http://e.org/p/'), '/tmp/q/')
+        self.assertEqual(m.src_to_dst('http://e.org/p/aa'), '/tmp/q/aa')
+        self.assertEqual(m.src_to_dst('http://e.org/p/aa/bb'), '/tmp/q/aa/bb')
+        self.assertEqual(m.src_to_dst(
+            'http://e.org/p/aa/bb/'), '/tmp/q/aa/bb/')
+        self.assertRaises(MapperError, m.src_to_dst, 'http://e.org/p')
+        self.assertRaises(MapperError, m.src_to_dst, 'http://e.org/pa')
+        self.assertRaises(MapperError, m.src_to_dst, 'nomatch')
 
     def test02_mapper_dst_to_src(self):
-        m=Mapper( ['http://e.org/p/','/tmp/q/'] )
-        self.assertEqual( m.dst_to_src('/tmp/q/'), 'http://e.org/p/')
-        self.assertEqual( m.dst_to_src('/tmp/q/bb'), 'http://e.org/p/bb')
-        self.assertEqual( m.dst_to_src('/tmp/q/bb/cc'), 'http://e.org/p/bb/cc')
-        self.assertRaises( MapperError, m.dst_to_src, '/tmp/q' )
-        self.assertRaises( MapperError, m.dst_to_src, '/tmp/qa')
-        self.assertRaises( MapperError, m.dst_to_src, 'nomatch' )
+        m = Mapper(['http://e.org/p/', '/tmp/q/'])
+        self.assertEqual(m.dst_to_src('/tmp/q/'), 'http://e.org/p/')
+        self.assertEqual(m.dst_to_src('/tmp/q/bb'), 'http://e.org/p/bb')
+        self.assertEqual(m.dst_to_src('/tmp/q/bb/cc'), 'http://e.org/p/bb/cc')
+        self.assertRaises(MapperError, m.dst_to_src, '/tmp/q')
+        self.assertRaises(MapperError, m.dst_to_src, '/tmp/qa')
+        self.assertRaises(MapperError, m.dst_to_src, 'nomatch')
 
     def test03_mapper2_src_to_dst(self):
-        m=Mapper( ['http://e.org/p=/tmp/q','http://e.org/r=/tmp/s'] )
-        self.assertEqual( m.src_to_dst('http://e.org/p/'), '/tmp/q/')
-        self.assertEqual( m.src_to_dst('http://e.org/p/aa'), '/tmp/q/aa')
-        self.assertEqual( m.src_to_dst('http://e.org/r/'), '/tmp/s/')
-        self.assertEqual( m.src_to_dst('http://e.org/r/aa'), '/tmp/s/aa')
+        m = Mapper(['http://e.org/p=/tmp/q', 'http://e.org/r=/tmp/s'])
+        self.assertEqual(m.src_to_dst('http://e.org/p/'), '/tmp/q/')
+        self.assertEqual(m.src_to_dst('http://e.org/p/aa'), '/tmp/q/aa')
+        self.assertEqual(m.src_to_dst('http://e.org/r/'), '/tmp/s/')
+        self.assertEqual(m.src_to_dst('http://e.org/r/aa'), '/tmp/s/aa')
 
     def test04_mapper2_dst_to_src(self):
-        m=Mapper( ['http://e.org/p=/tmp/q','http://e.org/r=/tmp/s'] )
-        self.assertEqual( m.dst_to_src('/tmp/q/'), 'http://e.org/p/')
-        self.assertEqual( m.dst_to_src('/tmp/q/bb'), 'http://e.org/p/bb')
-        self.assertEqual( m.dst_to_src('/tmp/s/'), 'http://e.org/r/')
-        self.assertEqual( m.dst_to_src('/tmp/s/bb'), 'http://e.org/r/bb')
+        m = Mapper(['http://e.org/p=/tmp/q', 'http://e.org/r=/tmp/s'])
+        self.assertEqual(m.dst_to_src('/tmp/q/'), 'http://e.org/p/')
+        self.assertEqual(m.dst_to_src('/tmp/q/bb'), 'http://e.org/p/bb')
+        self.assertEqual(m.dst_to_src('/tmp/s/'), 'http://e.org/r/')
+        self.assertEqual(m.dst_to_src('/tmp/s/bb'), 'http://e.org/r/bb')
 
     def test05_path_from_uri(self):
-        m=Mapper()
-        self.assertEqual( m.path_from_uri('a_file'), 'a_file' )
-        self.assertEqual( m.path_from_uri('some_path/a_file'), 'some_path/a_file' )
-        self.assertEqual( m.path_from_uri('http://localhost/p'), 'localhost_p' )
-        self.assertEqual( m.path_from_uri('http://localhost:8888/p'), 'localhost_8888_p' )
-        self.assertEqual( m.path_from_uri('https://localhost:8888/p'), 'localhost_8888_p' )
-        self.assertEqual( m.path_from_uri('http://example.com'), 'example.com' )
-        self.assertEqual( m.path_from_uri('http://example.com/'), 'example.com' )
-        self.assertEqual( m.path_from_uri('http://example.com/ex1'), 'example.com_ex1' )
-        self.assertEqual( m.path_from_uri('http://example.com/ex1/'), 'example.com_ex1' )
+        m = Mapper()
+        self.assertEqual(m.path_from_uri('a_file'), 'a_file')
+        self.assertEqual(m.path_from_uri(
+            'some_path/a_file'), 'some_path/a_file')
+        self.assertEqual(m.path_from_uri('http://localhost/p'), 'localhost_p')
+        self.assertEqual(m.path_from_uri(
+            'http://localhost:8888/p'), 'localhost_8888_p')
+        self.assertEqual(m.path_from_uri(
+            'https://localhost:8888/p'), 'localhost_8888_p')
+        self.assertEqual(m.path_from_uri('http://example.com'), 'example.com')
+        self.assertEqual(m.path_from_uri('http://example.com/'), 'example.com')
+        self.assertEqual(m.path_from_uri(
+            'http://example.com/ex1'), 'example.com_ex1')
+        self.assertEqual(m.path_from_uri(
+            'http://example.com/ex1/'), 'example.com_ex1')
 
     def test06_mapper_unsafe(self):
-        self.assertFalse( Mapper( ['http://example.com/=/tmp/a'] ).unsafe() )
-        self.assertFalse( Mapper( ['http://example.com/=http://example.com/'] ).unsafe() )
-        self.assertFalse( Mapper( ['http://example.com/'], use_default_path=True ).unsafe() )
+        self.assertFalse(Mapper(['http://example.com/=/tmp/a']).unsafe())
+        self.assertFalse(
+            Mapper(['http://example.com/=http://example.com/']).unsafe())
+        self.assertFalse(Mapper(['http://example.com/'],
+                                use_default_path=True).unsafe())
         # Following hits case of single local arg supplied
-        self.assertTrue( Mapper( ['/tmp/a'], use_default_path=True ).unsafe() )
+        self.assertTrue(Mapper(['/tmp/a'], use_default_path=True).unsafe())
         # One good, one bad -> bad
-        self.assertTrue( Mapper( ['http://example.com/=/tmp/a','/tmp/a=/tmp'] ).unsafe() )
+        self.assertTrue(
+            Mapper(['http://example.com/=/tmp/a', '/tmp/a=/tmp']).unsafe())
 
     def test07_default_src_uri(self):
-        self.assertEqual( Mapper(['a=b']).default_src_uri(), 'a')
-        self.assertEqual( Mapper(['a=b','b=c']).default_src_uri(), 'a')
-        self.assertRaises( MapperError, Mapper().default_src_uri )
-        
-    ##### Map #####
+        self.assertEqual(Mapper(['a=b']).default_src_uri(), 'a')
+        self.assertEqual(Mapper(['a=b', 'b=c']).default_src_uri(), 'a')
+        self.assertRaises(MapperError, Mapper().default_src_uri)
+
+    # Tests for Map class
 
     def test10_map_unsafe(self):
-        self.assertFalse( Map('http://example.com/','path').unsafe() )
-        # Note the first is a URI and the second is a (silly) path in the following
-        self.assertFalse( Map('http://example.com/','http://example.com/').unsafe() )
-        self.assertFalse( Map('a','b').unsafe() )
-        self.assertFalse( Map('path/a','path/b').unsafe() )
+        self.assertFalse(Map('http://example.com/', 'path').unsafe())
+        # Note the first is a URI and the second is a (silly) path in the
+        # following
+        self.assertFalse(
+            Map('http://example.com/', 'http://example.com/').unsafe())
+        self.assertFalse(Map('a', 'b').unsafe())
+        self.assertFalse(Map('path/a', 'path/b').unsafe())
         # The following are unsafe
-        self.assertTrue( Map('path','path').unsafe() )
-        self.assertTrue( Map('path/a','path').unsafe() )
-        self.assertTrue( Map('path','path/b').unsafe() )
+        self.assertTrue(Map('path', 'path').unsafe())
+        self.assertTrue(Map('path/a', 'path').unsafe())
+        self.assertTrue(Map('path', 'path/b').unsafe())
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestMapper)
