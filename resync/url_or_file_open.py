e3df46b37d3a450fb98981c1e20e3924a28d930b
Simeon Warner <simeon.warner@cornell.edu>
2020-12-21 14:55:39 -0500

    Add tests

--- resync/url_or_file_open.py
+++ resync/url_or_file_open.py
@@ -8,7 +8,7 @@ from . import __version__
 
 
 # Global configuration settings
-FIRST_REQUEST = False
+NUM_REQUESTS = 0
 CONFIG = {
     'bearer_token': None,
     'delay': None
@@ -22,20 +22,24 @@ def set_url_or_file_open_config(key, value):
 
 
 def url_or_file_open(uri):
-    """Wrapper around urlopen() to prepend file: if no scheme provided."""
+    """Wrapper around urlopen() to prepend file: if no scheme provided.
+
+    Can be used as a context manager because the return value from urlopen(...)
+    supports both that and straightforwrd use as simple file handle object.
+    """
     if (not re.match(r'''\w+:''', uri)):
         uri = 'file:' + uri
+    headers = {'User-Agent': 'resync/' + __version__}
     # Do we need to send an Authorization header?
     # FIXME - This token will be added blindy to all requests. This is insecure
     # if the --noauth setting is used allowing requests across different domains.
     # It would be better to have some scheme where a token is tied to a particular
     # domain, or domain pattern.
-    headers = {'User-Agent': 'resync/' + __version__}
     if CONFIG['bearer_token'] is not None:
         headers['Authorization'] = 'Bearer ' + CONFIG['bearer_token']
     # Have we got a delay set? Apply only to web requests after first
-    global FIRST_REQUEST
-    if not FIRST_REQUEST and CONFIG['delay'] is not None and not uri.startswith('file:'):
-        FIRST_REQUEST = False
+    global NUM_REQUESTS
+    if NUM_REQUESTS != 0 and CONFIG['delay'] is not None and not uri.startswith('file:'):
         time.sleep(CONFIG['delay'])
-    return(urlopen(Request(url=uri, headers=headers)))
+    NUM_REQUESTS += 1
+    return urlopen(Request(url=uri, headers=headers))
